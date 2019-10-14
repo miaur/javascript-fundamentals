c4b672167e89653d6d7f26b0ec83872a7a60d5ae
miaur <miaur.r@gmail.com>
2019-10-14 23:14:29 +0300

    Tests 1.1 and 1.2.

--- datatypes.test.js
+++ datatypes.test.js
@@ -1,12 +1,12 @@
 describe("Data types", () => {
     describe("Boolean", () => {
         it("Should define False and True boolean variables", () => {
-            var a = false;
-            var b = true;
+            let a = true;
+            let b = false;
 
-            expect(a).toBeFalsy();
+            expect(!a).toBeFalsy();
             expect(typeof a).toBe("boolean");
-            expect(b).toBeTruthy();
+            expect(!b).toBeTruthy();
             expect(typeof b).toBe("boolean");
         });
 
@@ -15,7 +15,7 @@ describe("Data types", () => {
             const TRUE = true;
 
             let useLogicalAnd = FALSE && TRUE;
-            let useLogicalOr = FALSE || TRUE;
+            let useLogicalOr = TRUE || FALSE;
             let useLogicalNot = !FALSE;
             let useDoubleLogicalNot = !!null;
 
@@ -32,9 +32,9 @@ describe("Data types", () => {
             const d = true;
 
             let cond1 = (a || c) && d;
-            let cond2 = (!a) && c && d;
-            let cond3 = a || (b && c);
-            let cond4 = !(c && d) || (!!null);
+            let cond2 = a || b || c;
+            let cond3 = (a || d) && !c;
+            let cond4 = !!((a || d) && !c);
 
             expect(cond1).toBe(true);
             expect(cond2).toBe(true);
@@ -44,12 +44,13 @@ describe("Data types", () => {
     });
 
     describe("Number", () => {
-        it.only("Should define different numbers", () => {
-            let a = 2;
-            let b = 0.5;
-            let c = NaN;
-            let d = Infinity;
-            let nan;
+        it("Should define different numbers", () => {
+            let a = 10;
+            let b = Infinity;
+            let c = 20.5;
+            let d = Number.MIN_SAFE_INTEGER;
+            let nan = NaN;
+
 
             expect(a).toBe(10);
             expect(!Number.isFinite(b)).toBe(true);
@@ -63,12 +64,12 @@ describe("Data types", () => {
             const a = 10;
             const b = 30;
 
-            expect(a + b).toBe( /* ??? */ );
-            expect( /* Put Expression Here */ ).toBe(-20);
-            expect( /* Put Expression Here */ ).toBe(20);
-            expect( /* Put Expression Here */ ).toBe(300);
-            expect( /* Put Expression Here */ ).toBe(3);
-            expect( /* Put Expression Here */ ).toBeCloseTo(0.333);
+            expect(a + b).toBe(40);
+            expect(a - b).toBe(-20);
+            expect(b - a).toBe(20);
+            expect(a * b).toBe(300);
+            expect(b / a).toBe(3);
+            expect(a / b).toBeCloseTo(0.333);
         });
 
         it("Should combine base operators", () => {
@@ -77,69 +78,75 @@ describe("Data types", () => {
             const c = 100;
 
             expect(a + b + c).toBe(140);
-            expect( /* Put Expression Here */ ).toBe(400);
-            expect( /* Put Expression Here */ ).toBe(2000);
-            expect( /* Put Expression Here */ ).toBe(3300);
-            // TODO: write 3 own test
+            expect((a + b) * a).toBe(400);
+            expect((b - a) * c).toBe(2000);
+            expect(b * c + c * 3).toBe(3300);
+            expect(c / a + b).toBe(40);
+            expect((a - b) / b).toBeCloseTo(-0.6667);
+            expect(a * b * c).toBe(30000);
         });
 
         it("Should convert to number", () => {
             expect(Number("123")).toBe(123);
             expect(Number("12.3")).toBe(12.3);
 
-            expect( /* Number(???) */ ).toBe(12);
-            expect( /* Number(???) */ ).toBe(12.3);
-            expect( /* Number(???) */ ).toBe(0);
-            expect( /* Number(???) */ ).toBe(0);
-            expect(Number("0b11")).toBe( /* ??? */ );
-            expect(Number("foo")).toBe("/* ??? */");
-            expect( /* Number(???*/ ).toBe(NaN);
-            expect(Number( /* "???" */ )).toBe(Number.NEGATIVE_INFINITY);
+            expect(Number("12")).toBe(12);
+            expect(Number("12.3")).toBe(12.3);
+            expect(Number("0")).toBe(0);
+            expect(Number("0b11")).toBe(3);
+            expect(Number("foo")).toBe(NaN);
+            expect(Number("")).toBe(0);
+            expect(Number("-Infinity")).toBe(Number.NEGATIVE_INFINITY);
         });
     });
 
     describe("String, object, null, undefined and symbols", () => {
         it("String", () => {
-            let str1; // Use single quote
-            let str2; // Use String(???)
-            let str3; // Use new String(???)
-            let str4; // Use template string and str1 variable
+            let str1 = 'string'; // Use single quote
+            let str2 = String(str1); // Use String(???)
+            let str3 = new String("string1"); // Use new String(???)
+            let str4 = `${str1}`; // Use template string and str1 variable
 
             expect(str1).toBe("string");
             expect(str1 === str2).toBeTruthy();
             expect(str1 === str3).toBeFalsy();
             expect(str1 === str4).toBeTruthy();
-            expect( /* use typeof */ ).toBe("object");
+            expect(typeof eval('({obj:"' + str1 + '"})')).toBe("object");
+
         });
 
         it("Should define object with 2 props", () => {
-            let obj; // Define object with 2 props
+            let obj = {
+                prop1: null,
+                prop2: undefined
+            }; // Define object with 2 props
 
             expect(typeof obj).toBe("object");
             expect(Object.keys(obj).length).toBe(2);
-
-            // TODO: write 2 own tests
+            expect(obj.prop1).toBeFalsy;
+            expect(String(obj.prop2)).toBe("undefined");
         });
 
         it("Should define variable with Null and undefined values", () => {
-            let nullVar; // Set null
-            let undefinedVar; // Set undefined
+            let nullVar = null; // Set null
+            let undefinedVar = undefined; // Set undefined
             let someVar; // Do not define it!!!
 
             expect(nullVar).toBeNull();
             expect(undefinedVar).toBe(undefinedVar);
             expect(someVar).toBe(undefined);
-            expect( /* typeof ??? */ ).toBe("object");
-            expect( /* typeof ??? */ ).toBe("undefined");
+            expect(typeof { str: "str1" }).toBe("object");
+            expect(String(typeof undefinedVar)).toBe("undefined");
         });
 
         it("Should define 2 Symbol variable with the same description", () => {
             const smbl1 = Symbol("test");
             const smbl2 = Symbol("test");
 
-            expect(typeof smbl1).toBe( /* ??? */ );
-            expect(typeof smbl2).toBe( /* ??? */ );
-            expect( /* Compare smbl1 and smbl2  */ ).toBe( /* ??? */ );
+            expect(typeof smbl1).toBe("symbol");
+            expect(typeof smbl2).toBe("symbol");
+            expect(smbl1.valueOf == smbl2.valueOf).toBe(true);
+            expect(smbl1 == smbl2).toBeFalsy(); //???
         });
     });
 });
--- function-basic.test.js
+++ function-basic.test.js
@@ -1,57 +1,96 @@
 describe("Function basic", () => {
-  it("Should be function declarations", () => {
-    expect(/* simpleFunction() */).toBe("I am a function declarations");
-  });
+    it("Should be function declarations", () => {
+        function simpleFunction() {
+            return ("I am a function declarations");
+        }
+        expect(simpleFunction()).toBe("I am a function declarations");
+    });
 
-  it("Should be function expression", () => {
-    expect(/* functionExpression() */).toBe("I am a function expression");
-  });
+    it("Should be function expression", () => {
+        var functionExpression = function() {
+            return "I am a function expression";
+        }
+        expect(functionExpression()).toBe("I am a function expression");
+    });
 
-  it("Should get sum of 2 numbers", () => {
-    expect(/* sum(5, 7) */).toBe(12);
-    expect(/* sum(10, 1000) */).toBe(1010);
-    expect(/* sum(-10, 10) */).toBe(0);
-  });
+    it("Should get sum of 2 numbers", () => {
+        function sum(a, b) {
+            return a + b;
+        }
+        expect(sum(5, 7)).toBe(12);
+        expect(sum(10, 1000)).toBe(1010);
+        expect(sum(-10, 10)).toBe(0);
+    });
 
-  test("Should get arguments length", () => {
-    // TODO: Declare 3 functions (withoutArguments, withOneArgument, withTwoArguments ) with different quantity of named arguments
-    expect(/* withoutArguments() */).toBe(0);
-    expect(/* withOneArgument(1) */).toBe(1);
-    expect(/* withTwoArguments(1, 2) */).toBe(2);
-    expect(/* withTwoArguments(1) */).toBe(1);
-    expect(/* withoutArguments(1, 2, 3, 4, 5) */).toBe(5);
-  });
+    test("Should get arguments length", () => {
+        function withoutArguments() {
+            return arguments.length;
+        }
 
-  test("Should find argument at N position", () => {
-    // Write function fn. First argument should be position (N) of argument
-    expect(/* fn(1) */).toBe(undefined);
-    expect(/* fn(1, "a") */).toBe("a");
-    expect(/* fn(3, "a", "b") */).toBe(undefined);
-    expect(/* fn(1, "a", "b", "c") */).toBe("a");
-    expect(/* fn(2, "a", "b", "c") */).toBe("b");
-    expect(/* fn(3, "a", "b", "c") */).toBe("c");
-  });
+        function withOneArgument(a) {
+            return arguments.length;
+        }
 
-  it("Should return string of wrapped arguments", () => {
-    expect(/* wrap("a", "b") */).toBe("|a|b|");
-    expect(/* wrap("a", "b", "c", "d", "e") */).toBe("|a|b|c|d|e|");
-  });
+        function withTwoArguments(a, b) {
+            return arguments.length;
+        }
+        expect(withoutArguments()).toBe(0);
+        expect(withOneArgument(1)).toBe(1);
+        expect(withTwoArguments(1, 2)).toBe(2);
+        expect(withTwoArguments(1)).toBe(1);
+        expect(withoutArguments(1, 2, 3, 4, 5)).toBe(5);
+    });
 
-  test("Should use Function as argument", () => {
-    // Write logCalculationResult function. Function should accept 2 arguments
-    // First is calculation function
-    // Second is argument for calculation function
-    // Return value is message 'Result is ###'
+    test("Should find argument at N position", () => {
+        // Write function fn. First argument should be position (N) of argument
+        function fn(n) {
+            return arguments[n];
+        }
+        expect(fn(1)).toBe(undefined);
+        expect(fn(1, "a")).toBe("a");
+        expect(fn(3, "a", "b")).toBe(undefined);
+        expect(fn(1, "a", "b", "c")).toBe("a");
+        expect(fn(2, "a", "b", "c")).toBe("b");
+        expect(fn(3, "a", "b", "c")).toBe("c");
+    });
 
-    function add10(a) {
-      return a + 10;
-    }
+    it("Should return string of wrapped arguments", () => {
+        function wrap() {
+            let symb = "|";
+            let argLenght = arguments.length;
+            let str = "";
+            if (argLenght > 0) {
+                str += symb;
+                for (let i = 0; i <= argLenght - 1; i++) {
+                    str += arguments[i] + symb;
+                }
+            }
+            return str;
+        }
 
-    function mul3(a) {
-      return a * 3;
-    }
+        expect(wrap("a", "b")).toBe("|a|b|");
+        expect(wrap("a", "b", "c", "d", "e")).toBe("|a|b|c|d|e|");
+    });
 
-    expect(/* logCalculationResult(add10, 7) */).toBe("Result is 17");
-    expect(/* logCalculationResult(mul3, 7) */).toBe("Result is 21");
-  });
+    test.only("Should use Function as argument", () => {
+        // Write logCalculationResult function. Function should accept 2 arguments
+        // First is calculation function
+        // Second is argument for calculation function
+        // Return value is message 'Result is ###'
+
+        function logCalculationResult(calcFunc, calcFuncArg) {
+            return "Result is " + calcFunc(calcFuncArg);
+        }
+
+        var f = function add10(a) {
+            return a + 10;
+        }
+
+        function mul3(a) {
+            return a * 3;
+        }
+
+        expect(logCalculationResult(f, 7)).toBe("Result is 17");
+        expect(logCalculationResult(mul3, 7)).toBe("Result is 21");
+    });
 });
