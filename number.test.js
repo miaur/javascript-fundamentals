754f6927a15072e4c1c288a0059429793f7c6806
miaur <miaur.r@gmail.com>
2019-10-16 11:27:34 +0300

    Test 1.3

--- number.test.js
+++ number.test.js
@@ -1,72 +1,136 @@
+import { argumentPlaceholder } from "@babel/types";
+
 describe("Numbers", () => {
-  it("Should use remainder operator '%' ", () => {
-    const a = 15;
-    const b = 10;
+    it("Should use remainder operator '%' ", () => {
+        const a = 15;
+        const b = 10;
 
-    expect(/* ??? */).toBe(5);
-    expect(/* ??? */).toBe(0);
-  });
+        function modulo(x, y) {
+            return x % y;
+        }
 
-  test("Should get average of 3 numbers", () => {
-    expect(/* average(2, 4, 6)*/).toBe(4);
-    expect(/* average(-5, 12, -7) */).toBe(0);
-  });
+        expect(modulo(a, b)).toBe(5);
+        expect(modulo(b, b)).toBe(0);
+    });
 
-  test("Should get a last digit of the number", () => {
-    expect(/* last(123) */).toBe(3);
-    expect(/* last(3982) */).toBe(2);
-  });
+    test("Should get average of 3 numbers", () => {
+        function average(a, b, c) {
+            return (a + b + c) / 3
+        }
+        expect(average(2, 4, 6)).toBe(4);
+        expect(average(-5, 12, -7)).toBe(0);
+    });
 
-  test("Should sum the digits of a given number", () => {
-    expect(/* sumDigits(123) */).toBe(6);
-    expect(/* sumDigits(53) */).toBe(8);
-  });
+    test("Should get a last digit of the number", () => {
+        function lastDigit(num) {
+            if (Number.isInteger(num)) {
+                return num % 10
+            } else {
+                return Number(String(num).slice(-1));
+            }
+            return -1;
+        }
 
-  test("Should return true if specified number is prime", () => {
-    expect(/* isPrime(7); */).toBe(true);
-    expect(/* isPrime(4) */).toBe(false);
+        expect(lastDigit(123)).toBe(3);
+        expect(lastDigit(3982)).toBe(2);
+        expect(lastDigit(3982.55)).toBe(5);
+    });
 
-    // TODO: Write additional tests
-  });
+    test("Should sum the digits of a given number", () => {
+        function sumDigits(num) {
+            let str = String(num);
+            let retStr = 0;
+            for (let i = 0; i < str.length; i++) {
+                if (str[i] != '.') {
+                    retStr += Number(str[i]);
+                }
+            }
+            return retStr;
+        }
 
-  test("Should convert string to number", () => {
-    expect(/* convert('234') */).toBe(234);
-  });
+        expect(sumDigits(123)).toBe(6);
+        expect(sumDigits(53)).toBe(8);
+        expect(sumDigits(53.7)).toBe(15);
+    });
 
-  test("Should find highest value", () => {
-    // TODO: Write 2 functions max and max2. Only one of them should use Math
+    test("Should return true if specified number is prime", () => {
+        function isPrime(n) {
+            if (n === 1) {
+                return false;
+            } else if (n === 2) {
+                return true;
+            } else {
+                for (let x = 2; x < n; x++) {
+                    if (n % x === 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        expect(isPrime(7)).toBe(true);
+        expect(isPrime(4)).toBe(false);
+        expect(isPrime(27)).toBe(false);
+    });
 
-    expect(/* max(1, 2) */).toBe(2);
-    expect(/* max2(1, 7, 2, 8, 5) */).toBe(8);
-  });
+    test("Should convert string to number", () => {
+        function convert(n) {
+            return Number(n);
+        }
+        expect(convert('234')).toBe(234);
+    });
 
-  test("Should find lowest value", () => {
-    expect(/* min(2, 3, 9, 4, 1, 5) */).toBe(1);
-    expect(/* min2(2, 3, 9, 4, 1, 5) */).toBe(1);
-    // TODO: Write additional tests
-  });
+    test.only("Should find highest value", () => {
+        // TODO: Write 2 functions max and max2. Only one of them should use Math
 
-  test("Should round up a value to the nearest integer", () => {
-    expect(/*  ??? 1 */).toBe(1);
-    expect(/* ??? 1.8 */).toBe(2);
-    expect(/* ??? 1.2 */).toBe(1);
-    expect(/* ??? -1.2 */).toBe(/* ??? */);
-  });
+        function max() {
+            return Math.max.apply(null, arguments);
+        }
 
-  test("Should get the largest integer less than or equal to a given number.  ", () => {
-    expect(/* ??? 1 */).toBe(1);
-    expect(/* ??? 1.2 */).toBe(1);
-    expect(/* ??? 1.8 */).toBe(1);
+        function max2() {
+            let maxNum;
+            if (arguments.length == 1) return arguments[0];
+            for (let i = 0; i < arguments.length - 1; i++) {
+                if (arguments[i] > arguments[i + 1]) {
+                    maxNum = arguments[i];
+                } else {
+                    maxNum = arguments[i + 1];
+                }
+            }
+            return maxNum;
+        }
 
-    // TODO: Write additional tests
-  });
+        expect(max(1, 2)).toBe(2);
+        expect(max2(1, 7, 2, 8, 5)).toBe(8);
+    });
 
-  test("Should return the base10 representation of a binary string", function() {
-    expect(/* ??? 11000000 */).toBe(192);
-  });
+    test("Should find lowest value", () => {
+        expect( /* min(2, 3, 9, 4, 1, 5) */ ).toBe(1);
+        expect( /* min2(2, 3, 9, 4, 1, 5) */ ).toBe(1);
+        // TODO: Write additional tests
+    });
 
-  test("Should convert an eight-bit string number to a binary string", function() {
-    expect(/* ??? 127 */).toBe("1010111");
-    expect(/* ??? 65 */).toBe("110101");
-  });
+    test("Should round up a value to the nearest integer", () => {
+        expect( /*  ??? 1 */ ).toBe(1);
+        expect( /* ??? 1.8 */ ).toBe(2);
+        expect( /* ??? 1.2 */ ).toBe(1);
+        expect( /* ??? -1.2 */ ).toBe( /* ??? */ );
+    });
+
+    test("Should get the largest integer less than or equal to a given number.  ", () => {
+        expect( /* ??? 1 */ ).toBe(1);
+        expect( /* ??? 1.2 */ ).toBe(1);
+        expect( /* ??? 1.8 */ ).toBe(1);
+
+        // TODO: Write additional tests
+    });
+
+    test("Should return the base10 representation of a binary string", function() {
+        expect( /* ??? 11000000 */ ).toBe(192);
+    });
+
+    test("Should convert an eight-bit string number to a binary string", function() {
+        expect( /* ??? 127 */ ).toBe("1010111");
+        expect( /* ??? 65 */ ).toBe("110101");
+    });
 });
